10c09bfe3eff8fa2ded84163c0dd381944bb5cf3
Mattias Giese <mattiasgiese@posteo.net>
2018-04-19 14:57:45 +1200

    build.sh: bump to current version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-YED_VERSION=${1:-3.17.2}
+YED_VERSION=${1:-3.18.0.2}
 BASE=${2:-oracle_jdk}
 docker build --build-arg YED_VERSION=${YED_VERSION} -f "Dockerfile.${BASE}" --rm -t "yed:${YED_VERSION}_${BASE}" .
 docker tag "yed:${YED_VERSION}_${BASE}" yed:latest
